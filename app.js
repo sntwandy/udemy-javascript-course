776b83be7e7cdf5a54eb3bc0898710861ed2fa89
sntwandy <sntwandy@gmail.com>
2020-05-10 08:43:08 -0400

    Learning about breakpoinst in JS

---
+++ app.js
@@ -0,0 +1,12 @@
+let a = 10,
+    b = 20,
+    c = 'Hello ',
+    d = 'Spiderman',
+    x = a + b;
+
+const greeting = c + d;
+
+//console.log({a});
+//console.log({a, b ,c ,d , x})
+
+c = 'Hello again!';
