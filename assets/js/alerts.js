09935353521115e4c802e691c689d9ae5bdc7126
sntwandy <sntwandy@gmail.com>
2020-05-11 15:21:07 -0400

    data type

---
+++ assets/js/alerts.js
@@ -0,0 +1,10 @@
+// Show you an Alert with a custom message.
+// alert('Hello, Im an alert');
+
+// Get an input from the user.
+//let name = prompt('What is your name?', 'Name');
+//console.log(name);
+
+// Get an input from the user, only both values 'True' or 'False'.
+//let option = confirm('Are you sure you want to cancel?');
+//console.log(option);
---
+++ assets/js/primitivos.js
@@ -0,0 +1,41 @@
+// Strings
+let name = 'Megan Herrera'
+console.log(name);
+
+name = 'Wandy Santana';
+console.log(name)
+
+name = "Zeus Amenadiel";
+name = `Darcy Bartolome`;
+
+console.log(typeof name);
+name = 17;
+console.log(typeof name);
+
+// Boolean
+let isMarvel = false;
+console.log(isMarvel);
+console.log(typeof isMarvel);
+
+// Numbers
+let age = 21;
+console.log(age);
+
+age = 21.563
+console.log(typeof age);
+
+// Undifined
+let superPower;
+console.log(typeof superPower);
+
+// Null
+let imNull = null;
+console.log(typeof imNull);
+
+// Symbol
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol1);
+
+console.log(symbol1 === symbol2);
