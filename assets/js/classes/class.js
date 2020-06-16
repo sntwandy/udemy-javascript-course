fbac0bd5f014bcc9df51700402d32003feba62bf
sntwandy <sntwandy@gmail.com>
2020-06-16 04:24:31 -0400

    Methods

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -8,6 +8,14 @@ class Person {
         this.code = code;
         this.phrase = phrase;
     };
+
+    whoIam() {
+        console.log(`I am ${this.name} and my identity ${this.code}`);
+    };
+
+    myPhrase() {
+        console.log(`${this.code} says: ${this.phrase}`);
+    };
 };
 
 const darcy = new Person('Darcy', 'Spider', 'I am Darcy');
@@ -15,3 +23,9 @@ const zeus = new Person('Zeus', 'Iron', 'I am Zeus');
 
 console.log(darcy);
 console.log(zeus);
+
+darcy.whoIam();
+zeus.whoIam();
+
+darcy.myPhrase();
+zeus.myPhrase();
