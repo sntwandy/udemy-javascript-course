8f9ca9553c0cbd3bdde0c9f29d6b79a4aaf5541c
sntwandy <sntwandy@gmail.com>
2020-06-16 04:57:19 -0400

    Sets and Gets inside a class

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -1,12 +1,21 @@
 class Person {
-    name = '';
-    code = '';
-    phrase = '';
+    // name = '';
+    // code = '';
+    // phrase = '';
+    food = '';
 
     constructor(name = 'No name', code = 'No code', phrase = 'No phrase') {
         this.name = name;
         this.code = code;
         this.phrase = phrase;
+    };
+
+    set setFavoriteFood(food) {
+        this.food = food.toUpperCase();
+    };
+
+    get getFavoriteFood() {
+        return `Favorite food of ${this.name} is ${this.food}`;
     };
 
     whoIam() {
@@ -21,11 +30,15 @@ class Person {
 const darcy = new Person('Darcy', 'Spider', 'I am Darcy');
 const zeus = new Person('Zeus', 'Iron', 'I am Zeus');
 
-console.log(darcy);
 console.log(zeus);
 
 darcy.whoIam();
 zeus.whoIam();
 
-darcy.myPhrase();
-zeus.myPhrase();
+// darcy.myPhrase();
+// zeus.myPhrase();
+
+darcy.setFavoriteFood = 'Spaguetti';
+console.log(darcy);
+
+console.log(darcy.getFavoriteFood);
